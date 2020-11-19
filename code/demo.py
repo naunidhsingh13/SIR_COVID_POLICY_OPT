afa34cc70d29d72a5ec59aa06c08bdbc20951d25
naunidhsingh13 <nsingh28@hawk.iit.edu>
2020-11-18 23:59:10 -0600

    Modified Optimizer

--- code/demo.py
+++ code/demo.py
@@ -2,17 +2,21 @@
 from Optimizer import StateOptimiser
 from Processor import Processor
 #
-obj = StateOptimiser("Illinois")
-
-obj.optimise_state_data()
-
-d1, d2 = obj.get_projection_data({})
-for x,y in zip(d1, d2):
-    print(x, " : ", y)
-print(obj.theta)
-
+# obj = StateOptimiser("Illinois")
+#
+# obj.optimise_state_data()
+#
+# d1, d2 = obj.get_projection_data({})
+# for x,y in zip(d1, d2):
+#     print(x, " : ", y)
+# print(obj.theta)
+#
 # obj.dump()
 #
-# p = Processor()
-# p.load_all_states()
+# #
+p = Processor()
+p.load_all_states()
 # print(p.get_analysis_of("Illinois", {}))
+t1, t2, t3, t4 = p.get_state_analysis_with_policy_list("Illinois", [], [])
+print(t1.values.tolist())
+
